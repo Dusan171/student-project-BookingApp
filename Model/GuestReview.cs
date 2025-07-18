35583a97dad24f9b9d8a85aafaaecef2222da6ce
Dusan <proba@nesto.com>
2025-07-18 10:07:18 +0200

    feature: create GuestReview class

---
+++ Model/GuestReview.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BookingApp.Serializer;
+
+namespace BookingApp.Model
+{
+    internal class GuestReview : ISerializable
+    {
+        public int Id { get; set; }
+        public int ReservationId { get; set; }
+
+        public int CleanlinessRating { get; set; }
+        public int OwnerRating { get; set; }
+        public string Comment { get; set; }
+        public string ImagePaths { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+        public GuestReview() { }
+
+        public GuestReview(int id, int reservationId, int cleanliness, int owner, string comment, string imagePaths)
+        {
+            Id = id;
+            ReservationId = reservationId;
+            CleanlinessRating = cleanliness;
+            OwnerRating = owner;
+            Comment = comment;
+            ImagePaths = imagePaths;
+            CreatedAt = DateTime.Now;
+        }
+        public string[] ToCSV()
+        {
+            return new string[]
+            {
+                Id.ToString(),
+                ReservationId.ToString(),
+                CleanlinessRating.ToString(),
+                OwnerRating.ToString(),
+                Comment,
+                ImagePaths,
+            };
+        }
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            ReservationId= int.Parse(values[1]);
+            CleanlinessRating = int.Parse(values[2]);
+            OwnerRating = int.Parse(values[3]);
+            Comment = values[4];
+            ImagePaths = values[5];
+            CreatedAt = DateTime.Parse(values[6]);
+        }
+    }
+}
