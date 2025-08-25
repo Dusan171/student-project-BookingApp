e67bac7c1dedd72de677a1d916caf8b6ff7d634d
anciim <markovicana505@gmail.com>
2025-08-25 21:32:32 +0200

    Making changes in repository classes and .csv files

--- Repositories/ReservationGuestRepository.cs
+++ Repositories/ReservationGuestRepository.cs
@@ -8,12 +8,10 @@ using BookingApp.Serializer;
 
 namespace BookingApp.Repositories
 {
-    internal class ReservationGuestRepository
+    public class ReservationGuestRepository
     {
-        private const string FilePath = "C:/Users/PC/Desktop/5 semestar/sims-projekat/sims-ra-2025-group-7-team-b/Resources/Data/reservationGuests.csv";
-
+        private const string FilePath = "../../Resources/Data/reservationGuests.csv";
         private readonly Serializer<ReservationGuest> _serializer;
-
         private List<ReservationGuest> _guests;
 
         public ReservationGuestRepository()
@@ -22,53 +20,47 @@ namespace BookingApp.Repositories
             _guests = _serializer.FromCSV(FilePath);
         }
 
-        public List<ReservationGuest> GetAll()
-        {
-            return _serializer.FromCSV(FilePath);
-        }
-
-        public ReservationGuest Save(ReservationGuest guest)
-        {
-            guest.Id = NextId();
-            _guests = _serializer.FromCSV(FilePath);
-            _guests.Add(guest);
-            _serializer.ToCSV(FilePath, _guests);
-            return guest;
-        }
-
+        // DODANA metoda za generisanje ID-a
         public int NextId()
         {
-            _guests = _serializer.FromCSV(FilePath);
             if (_guests.Count < 1)
-            {
                 return 1;
-            }
             return _guests.Max(g => g.Id) + 1;
         }
 
-        public void Delete(ReservationGuest guest)
+        public List<ReservationGuest> GetAll()
         {
-            _guests = _serializer.FromCSV(FilePath);
-            ReservationGuest found = _guests.Find(g => g.Id == guest.Id);
-            _guests.Remove(found);
-            _serializer.ToCSV(FilePath, _guests);
-        }
-
-        public ReservationGuest Update(ReservationGuest guest)
-        {
-            _guests = _serializer.FromCSV(FilePath);
-            ReservationGuest current = _guests.Find(g => g.Id == guest.Id);
-            int index = _guests.IndexOf(current);
-            _guests.Remove(current);
-            _guests.Insert(index, guest);
-            _serializer.ToCSV(FilePath, _guests);
-            return guest;
+            return _guests;
         }
 
         public List<ReservationGuest> GetByReservationId(int reservationId)
         {
-            _guests = _serializer.FromCSV(FilePath);
-            return _guests.FindAll(g => g.ReservationId == reservationId);
+            return _guests.Where(g => g.ReservationId == reservationId).ToList();
+        }
+
+        public ReservationGuest GetById(int id)
+        {
+            return _guests.FirstOrDefault(g => g.Id == id);
+        }
+
+        public void SaveAll()
+        {
+            _serializer.ToCSV(FilePath, _guests);
+        }
+
+        // IZMENA: Dodato generisanje ID-a pre dodavanja
+        public ReservationGuest Add(ReservationGuest guest)
+        {
+            guest.Id = NextId();
+            _guests.Add(guest);
+            SaveAll();
+            return guest;
+        }
+
+        public void RemoveByReservationId(int reservationId)
+        {
+            _guests.RemoveAll(g => g.ReservationId == reservationId);
+            SaveAll();
         }
     }
 }
