43229c6c40113a0caf937a74ffd4168439f3c734
Aleksandra <aleksandrarudez12@gmail.com>
2025-10-01 19:53:52 +0200

    Fix merge conflicts

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,39 +1,34 @@
-﻿using System.Windows;
+﻿using System;
+using System.Text;
+using System.Windows;
 using System.Windows.Threading;
 using BookingApp.Services;
-using System.Windows.Threading;
-using System.Text;
 
 namespace BookingApp
 {
     public partial class App : Application
     {
-        public App()
+        protected override void OnStartup(StartupEventArgs e)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            InitializeComponent();
-        }
-        protected override void OnStartup(StartupEventArgs e)
-        {
             base.OnStartup(e);
 
             LocalizationService.Instance.SetLanguage("en-US");
 
-             this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
         }
-         void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-         {
-             string errorMessage = $"An unhandled exception occurred: \n\n" +
-                                   $"Message: {e.Exception.Message}\n\n" +
-                                   $"Stack Trace: \n{e.Exception.StackTrace}";
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string errorMessage = $"An unhandled exception occurred: \n\n" +
+                                  $"Message: {e.Exception.Message}\n\n" +
+                                  $"Stack Trace: \n{e.Exception.StackTrace}";
 
-             MessageBox.Show(errorMessage, "Critical Application Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show(errorMessage, "Critical Application Error", MessageBoxButton.OK, MessageBoxImage.Error);
 
-             e.Handled = true;
+            e.Handled = true;
 
-              Application.Current.Shutdown();
-         }
+            Application.Current.Shutdown();
+        }
     }
 }
-
