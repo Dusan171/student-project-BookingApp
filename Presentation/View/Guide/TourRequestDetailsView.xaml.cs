c6620aba799e8525787a8884926e1b115ce9b97f
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-10-06 15:56:40 +0200

    Creating TourRequestDetailsView

---
+++ Presentation/View/Guide/TourRequestDetailsView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using BookingApp.Domain.Model;
+using BookingApp.Presentation.ViewModel.Guide;
+
+namespace BookingApp.Presentation.View.Guide
+{
+    public partial class TourRequestDetailsPage : Page
+    {
+        public TourRequestDetailsPage(TourRequest request, TourRequestsViewModel parentVM)
+        {
+            InitializeComponent();
+
+            var vm = new TourRequestDetailsViewModel(request);
+            vm.TourAccepted += (acceptedTour) =>
+            {
+                parentVM.Requests.Remove(acceptedTour);
+
+                if (NavigationService?.CanGoBack == true)
+                    NavigationService.GoBack();
+            };
+
+            DataContext = vm;
+        }
+    }
+
+}
