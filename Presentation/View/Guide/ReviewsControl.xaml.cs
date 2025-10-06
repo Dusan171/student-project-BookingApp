80aae8b9ca313141aec7f3c299a2cfb99235520a
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-10-06 15:57:57 +0200

    Turning ReviewsControl into Page

--- Presentation/View/Guide/ReviewsControl.xaml.cs
+++ Presentation/View/Guide/ReviewsControl.xaml.cs
@@ -1,123 +1,14 @@
-﻿using BookingApp.Domain;
-using BookingApp.Domain.Model;
-using BookingApp.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
+using BookingApp.Presentation.ViewModel.Guide;
 
 namespace BookingApp.Presentation.View.Guide
 {
-    public partial class ReviewsControl : UserControl 
+    public partial class ReviewsControl : Page
     {
-        private List<TourReview> reviews;
-        private List<TouristAttendance> attendances;
-        private List<Tour> tours;
-        private List<ReservationGuest> guests;
-        private TourReviewRepository reviewRepository;
-        private KeyPointRepository keypointRepository;
-        private TouristAttendanceRepository attendancesRepository;
-        private ReservationGuestRepository guestsRepository;
-        private UserRepository userRepository;
-        private List<User> tourists;
-        private TourRepository toursRepository;
-        private ObservableCollection<TourReviewDisplayDTO> reviewDisplays;
-
         public ReviewsControl()
         {
             InitializeComponent();
-            reviewRepository = new TourReviewRepository();
-            toursRepository = new TourRepository();
-            attendancesRepository = new TouristAttendanceRepository();
-            userRepository = new UserRepository();
-            guestsRepository = new ReservationGuestRepository();
-            keypointRepository = new KeyPointRepository();
-            reviewDisplays = new ObservableCollection<TourReviewDisplayDTO>();
-
-            LoadReviews();
-            LoadAttendaces();
-            LoadTourists();
-            LoadGuests();
-            LoadTours();
-            CreateReviewForShow();
-        }
-
-        private void LoadReviews()
-        {
-            reviews = reviewRepository.GetAll(); 
-        }
-        private void LoadAttendaces()
-        {
-            attendances = attendancesRepository.GetAll();
-        }
-        private void LoadTourists()
-        {
-            tourists = userRepository.GetAll();
-            tourists = tourists.Where(t => t.Role == UserRole.TOURIST).ToList();
-        }
-        private void LoadGuests()
-        {
-            guests = guestsRepository.GetAll();
-        }
-        public void LoadTours()
-        {
-            tours = toursRepository.GetAll();
-            if (tours.Count() == 0)
-            {
-                MessageBox.Show("nema tura");
-            }
-        }
-        private void CreateReviewForShow()
-        {
-            //ime turiste, ime ture, keypoint, sve ove ocene podaci
-            foreach(var review in reviews)
-            {
-                
-                var tour = tours.FirstOrDefault(t => t.Id == review.TourId);
-                var tourist = tourists.FirstOrDefault(t => t.Id == review.TouristId);
-
-                if (tour == null || tourist == null)
-                {
-                    continue;
-                }
-
-                String joinedAt = GetKeyPointJoinedAt(tourist.Id, guests);
-                TourReviewDisplayDTO reviewDisplay = new TourReviewDisplayDTO(1, tour.Name, review.IsValid, tourist.FirstName + " " + tourist.LastName, joinedAt, review.GuideKnowledge, review.GuideLanguage, review.TourInterest, review.Comment, review);
-                reviewDisplays.Add(reviewDisplay);
-                //dodaj u fajl preko repository, a mozda i ne mora fr
-            }
-            ReviewsList.ItemsSource = null;
-            ReviewsList.ItemsSource = reviewDisplays; 
-
-        }
-        private String GetKeyPointJoinedAt(int touristId, List<ReservationGuest> guests)
-        {
-            var guest = guests.FirstOrDefault(g => g.TouristId == touristId);
-            var kp = keypointRepository.GetById(guest.KeyPointJoinedAt);
-            return kp.Name;
-        }
-        private void ReportButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button && button.DataContext is TourReviewDisplayDTO review)
-            {
-                var result = MessageBox.Show("Da li ste sigurni da želite da prijavite ovu recenziju?",
-                                             "Potvrda prijave",
-                                             MessageBoxButton.YesNo,
-                                             MessageBoxImage.Question);
-
-                if (result == MessageBoxResult.Yes)
-                {
-                    
-                    review.OriginalReview.IsValid = false;
-                    review.IsValid = false;
-                    reviewRepository.UpdateReview(review.OriginalReview); 
-                    reviewRepository.SaveToFile();
-                    //CreateReviewForShow();
-                    ReviewsList.Items.Refresh();
-                }
-            }
+            DataContext = new ReviewsControlViewModel();
         }
     }
 }
