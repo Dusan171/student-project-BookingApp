172bd20556e1d34ca1310ebbca8e8f073bc9ac6f
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-10-06 15:09:29 +0200

    Adding detiled tour page

---
+++ Presentation/View/Guide/DetailedTourPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BookingApp.Domain.Model;
+using System.Windows.Controls;
+
+namespace BookingApp.Presentation.View.Guide
+{
+    public partial class DetailedTourPage : Page
+    {
+        public DetailedTourPage(Tour tour)
+        {
+            InitializeComponent();
+            DataContext = new DetailedTourViewModel(tour);
+        }
+    }
+}
+
