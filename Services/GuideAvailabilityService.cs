657b11923246d7b2445ca2d14a3d79eb342a9ebb
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-10-07 07:00:44 +0200

    Updating IsAvailable

--- Services/GuideAvailabilityService.cs
+++ Services/GuideAvailabilityService.cs
@@ -4,44 +4,44 @@ using System.Linq;
 using BookingApp.Domain.Model;
 using BookingApp.Repositories;
 
-
 namespace BookingApp.Services
 {
     public class GuideAvailabilityService 
     {
-        private readonly Dictionary<int, List<(DateTime start, DateTime end)>> _assignments = new Dictionary<int, List<(DateTime, DateTime)>>();
-        private TourRepository _toursRepo = new TourRepository();
+        private readonly TourRepository _tourRepository;
 
+        public GuideAvailabilityService()
+        {
+            _tourRepository = new TourRepository();
+        }
 
         public bool IsAvailable(int guideId, DateTime start, DateTime end)
         {
-            if (!_assignments.ContainsKey(guideId)) return true;
-            var assigned = _assignments[guideId];
-            return !assigned.Any(a => start < a.end && end > a.start);
-        }
+            if (end <= start)
+                return false;
 
-        public void FillGuideWithTours(int id)
-        {
-            List<Tour> tours = _toursRepo.GetAll().Where(t => t.GuideId == id).ToList();
-            foreach(var tour in tours)
+            var guideTours = _tourRepository.GetAll()
+                .Where(t => t.GuideId == guideId)
+                .ToList();
+
+            foreach (var tour in guideTours)
             {
-                AddTourToGuide(id, tour);
+                if (tour.StartTimes == null || !tour.StartTimes.Any())
+                    continue;
+
+                foreach (var startTime in tour.StartTimes)
+                {
+                    var tourStart = startTime.Time;
+                    var tourEnd = tourStart.AddHours(tour.DurationHours);
+                    
+                    if (start < tourEnd && end > tourStart)
+                    {
+                        return false;
+                    }
+                }
             }
-        }
 
-        public void AddTourToGuide(int guideId, Tour tour)
-        {
-            if (!_assignments.ContainsKey(guideId)) _assignments[guideId] = new List<(DateTime, DateTime)>();
-            // For simplicity assume tour.StartTimes has one time and DurationHours set
-            var start = tour.StartTimes.FirstOrDefault()?.Time ?? DateTime.Now;
-            var end = start.AddHours(tour.DurationHours);
-            _assignments[guideId].Add((start, end));
-        }
-
-
-        public void RemoveTourFromGuide(int guideId, int tourId)
-        {
-            // no-op for simple store
+            return true; 
         }
     }
 }
