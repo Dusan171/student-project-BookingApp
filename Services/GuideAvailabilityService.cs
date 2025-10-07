f774c00fe0e26742d57b9893e2fccb396425f2d9
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-10-07 15:41:26 +0200

    Adding IsComplexTourPartConfilctFree

--- Services/GuideAvailabilityService.cs
+++ Services/GuideAvailabilityService.cs
@@ -9,10 +9,12 @@ namespace BookingApp.Services
     public class GuideAvailabilityService 
     {
         private readonly TourRepository _tourRepository;
+        private readonly ComplexTourRequestPartRepository _complexPartRepository;
 
         public GuideAvailabilityService()
         {
             _tourRepository = new TourRepository();
+            _complexPartRepository = new ComplexTourRequestPartRepository();
         }
 
         public bool IsAvailable(int guideId, DateTime start, DateTime end)
@@ -37,6 +39,33 @@ namespace BookingApp.Services
                     if (start < tourEnd && end > tourStart)
                     {
                         return false;
+                    }
+                }
+            }
+
+            return true; 
+        }
+
+        public bool IsComplexTourPartConflictFree(int complexTourRequestId, int currentPartId, DateTime start, DateTime end)
+        {
+            if (end <= start)
+                return false;
+
+            var allParts = _complexPartRepository.GetByComplexRequestId(complexTourRequestId);
+            
+            foreach (var part in allParts)
+            {
+                if (part.Id == currentPartId)
+                    continue;
+
+                if (part.Status == TourRequestStatus.ACCEPTED && part.ScheduledDate.HasValue)
+                {
+                    var partStart = part.ScheduledDate.Value;
+                    var partEnd = partStart.AddHours(2); 
+                    
+                    if (start < partEnd && end > partStart)
+                    {
+                        return false; 
                     }
                 }
             }
