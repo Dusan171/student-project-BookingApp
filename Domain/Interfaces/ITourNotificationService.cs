7c21d069bc6628c17107217dbee259429b7bf2ba
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-10-07 06:55:48 +0200

    Update ITourNotificationService.cs

--- Domain/Interfaces/ITourNotificationService.cs
+++ Domain/Interfaces/ITourNotificationService.cs
@@ -1,4 +1,5 @@
 ﻿using BookingApp.Services.DTO;
+using System;
 using System.Collections.Generic;
 
 namespace BookingApp.Domain.Interfaces
@@ -8,6 +9,7 @@ namespace BookingApp.Domain.Interfaces
         List<TourNotificationDTO> GetNotificationsByTourist(int touristId);
         TourNotificationDTO GetLatestNotification(int touristId);
         void CreateNotificationsForNewTour(int tourId, string tourName, string location, string language);
+        void SendTourAcceptanceNotification(int touristId, int tourId, string tourName, string location, DateTime scheduledDate, string guideName);
         void MarkAsRead(int notificationId);
     }
 }
