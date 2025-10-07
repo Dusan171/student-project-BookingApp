bf7ce17e0b0ab242acd6c6f8a3a502365416d42c
anciim <markovicana505@gmail.com>
2025-10-07 21:39:19 +0200

    Domain Model classes which were missing

---
+++ Domain/Model/LanguageStatisticData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Domain.Model
+{
+    public class LanguageStatisticData
+    {
+        public string Language { get; set; }
+        public int Count { get; set; }
+        public double BarWidth { get; set; }
+    }
+}
